66524f1e221ff73b09824fed4655dcae0f450d24
rnbsrva <orynbasarovaakerke88@gmail.com>
2023-11-10 00:15:21 +0600

    qr service'

---
+++ src/app/core/service/qr.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {EnvService} from "./env.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QrService {
+
+  private QR_URL: string;
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly env: EnvService
+  ) {
+    this.QR_URL = env.getApiUrl().concat('qr/')
+  }
+
+  generate(
+    data: string
+  ): Observable<any> {
+    return this.http.get(`${this.QR_URL}generate`, {
+      params: {data},
+      responseType: 'blob'
+    })
+  }
+
+
+}
